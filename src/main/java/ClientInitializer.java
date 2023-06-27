27a1dc34ccffe418a328537a61863021a0172ee9
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-06-27 18:54:44 +0530

    Chat Project Addes

---
+++ src/main/java/ClientInitializer.java
@@ -0,0 +1,28 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Objects;
+
+public class ClientInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/login.fxml"));
+        Scene mainScene = new Scene(root);
+        stage.setScene(mainScene);
+        stage.setTitle("Java FX POS");
+        stage.centerOnScreen();
+        stage.show();
+
+
+    }
+}
