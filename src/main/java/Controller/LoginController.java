8f14c5d3fb4b501dc863e5e6e84e475ab117ddb2
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-06-27 20:37:13 +0530

    Create LoginController send ClientController

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -2,15 +2,30 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
     @FXML
     private TextField userName;
+   // public JFXTextField Txt_Enter_Name;
+    static String User_name;
 
     @FXML
-    void NameOnAction(ActionEvent event) {
+    void NameOnAction(ActionEvent event) throws IOException {
+
+            User_name=userName.getText();
+            userName.clear();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(LoginController.class.getResource("/view/client.fxml"))));
+            stage.close();
+            stage.centerOnScreen();
+            stage.show();
 
     }
 
